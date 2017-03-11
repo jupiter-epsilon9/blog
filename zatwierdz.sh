00541197fd905d14c1360371806047cde60b28ae
Jupiter <jupiter.epsilon.9@gmail.com>
2017-03-11 16:53:39 +0100

    Fix

--- zatwierdz.sh
+++ zatwierdz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 hugo -t hugo-redlounge -b "http://jupiter.098.pl/"
 git add . --all
-echo -n "Komentarz do zmian > "
+echo -en "\e[1m\e[93mKomentarz do zmian > "
 read comment1
 git commit -m "$comment1"
 git push
