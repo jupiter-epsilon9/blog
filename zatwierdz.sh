118aa28a808c43dd8b387a0d5a3972c62e1650ff
Jupiter <jupiter.epsilon.9@gmail.com>
2017-03-11 16:56:42 +0100

    fix

--- zatwierdz.sh
+++ zatwierdz.sh
@@ -3,6 +3,6 @@ hugo -t hugo-redlounge -b "http://jupiter.098.pl/"
 git add . --all
 echo -ne "\e[1m\e[93mKomentarz do zmian > "
 read comment1
-echo -e "\e[0m"
+echo -ne "\e[0m"
 git commit -m "$comment1"
 git push
