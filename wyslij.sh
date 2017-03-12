5bf5f7a8d255eb1b76705b2a3ddd87ddc4d7f12a
Jupiter <jupiter.epsilon.9@gmail.com>
2017-03-12 17:48:21 +0100

    www

--- wyslij.sh
+++ wyslij.sh
@@ -2,8 +2,8 @@
 git ftp push --syncroot public
 if [ $? -eq "8" ]; then
         echo -e "\e[1m\e[31mBłąd przy wysyłaniu na serwer. Spróbuj uruchomić ./zatwierdz.sh\e[0m"
-elif [ $? -ne "0" ] ; then
-        echo -e "\e[1m\e[31mBłąd przy wysyłaniu na serwer.\e[0m"
+elif [ $? -eq "0" ] ; then
+        echo "Wysłano."
 else
-    echo "Wysłano."
+        echo -e "\e[1m\e[31mBłąd przy wysyłaniu na serwer.\e[0m"
 fi
