20d06e9817d03b54b8a227773e09f17b6e4170f3
Jupiter <jupiter.epsilon.9@gmail.com>
2017-03-12 17:40:26 +0100

    fix

--- TECH.md
+++ TECH.md
@@ -92,8 +92,8 @@ cat << EOF > edytuj.sh
         echo -e "\e[1m\e[31Błąd aktualizacji repozytorium GIT\e[0m"
         exit
 	fi
-	cmd "/C start /I C:/\"Program Files (x86)/MarkdownPad 2\"/MarkdownPad2.exe"
-	cmd "/C start /I C:/\"Program Files (x86)/Google/Chrome/Application\"/chrome.exe http://localhost/"	
+	cmd "/C start /I C:/\"Program Files (x86)/MarkdownPad 2\"/MarkdownPad2.exe" &
+	cmd "/C start /I C:/\"Program Files (x86)/Google/Chrome/Application\"/chrome.exe http://localhost/"	&
 	hugo server -t hugo-redlounge -p 80 -b "http://localhost/"
 EOF
 chmod +x edytuj.sh
--- edytuj.sh
+++ edytuj.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git pull
 if [ $? -ne 0 ]; then
-        echo "Błąd aktualizacji repozytorium GIT"
+        echo -e "\e[1m\e[31Błąd aktualizacji repozytorium GIT\e[0m"
         exit
 fi
-cmd "/C start /I C:/\"Program Files (x86)/MarkdownPad 2\"/MarkdownPad2.exe"
-cmd "/C start /I C:/\"Program Files (x86)/Google/Chrome/Application\"/chrome.exe http://localhost/"
+cmd "/C start /I C:/\"Program Files (x86)/MarkdownPad 2\"/MarkdownPad2.exe" &
+cmd "/C start /I C:/\"Program Files (x86)/Google/Chrome/Application\"/chrome.exe http://localhost/"&
 hugo server -t hugo-redlounge -p 80 -b "http://localhost/"
